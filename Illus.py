ecd60d87d3d1c4f83c62c08a7acdf4b5e461e273
Shubham Mishra <donofsuri@gmail.com>
2017-02-27 13:44:54 +0530

    Added color settings

--- Illus.py
+++ Illus.py
@@ -19,23 +19,60 @@ from Tkinter import *
 class RtDialog(Label):
     def __init__(self, root, color, pwidth, x, y):
         Label.__init__(self, root, height=40, width=40)
-        self.colors = ('White', 'Black', 'Blue', 'Beige',\
-                       'Cyan', 'Magenta', 'Green', 'Red', 'Orange', 'Brown',\
-                       'Yellow')
-        self.menu = apply(OptionMenu, (self, color) + self.colors)
-        self.menu.pack()
-        
+        #self.colors = ('White', 'Black', 'Blue', 'Beige',\
+        #               'Cyan', 'Magenta', 'Green', 'Red', 'Orange', 'Brown',\
+        #               'Yellow')
+        #self.menu = apply(OptionMenu, (self, color) + self.colors)
+        #self.menu.pack()
+        self.title1 = Label(self, text='Color\n__________').pack()
+        self.title2 = Label(self, text='Red').pack()
+        self.red = Scale(self, from_=0, to=255, orient=HORIZONTAL)
+        self.red.pack() 
+        self.title3 = Label(self, text='Green').pack()
+        self.green =  Scale(self, from_=0, to=255, orient=HORIZONTAL)
+        self.green.pack()
+        self.title4 = Label(self, text='Blue').pack()
+        self.blue =  Scale(self, from_=0, to=255, orient=HORIZONTAL)
+        self.blue.pack()
+        self.red.bind('<Button-1>', self.assigncolor)
+        self.red.bind('<B1-Motion>', self.assigncolor)
+        self.green.bind('<Button-1>', self.assigncolor)
+        self.green.bind('<B1-Motion>', self.assigncolor)
+        self.blue.bind('<Button-1>', self.assigncolor)
+        self.blue.bind('<B1-Motion>', self.assigncolor)
+       
+        self.color = color 
         self.pwidth = pwidth
+        
+        self.title5 = Label(self, text='Width\n__________').pack()
         self.wcontrol = Scale(self, from_=1, to=100, orient=HORIZONTAL)
         self.wcontrol.bind('<Button-1>', self.assignwidth)
         self.wcontrol.bind('<B1-Motion>', self.assignwidth)
-        self.wcontrol.set(pwidth.get())
         self.wcontrol.pack()
 
+        self.defaultset()
+        self.colorbar = Label(self, text='Illus', width=10, background='Black', fg=self.color.get())
+        self.colorbar.pack()
+        
         self.place(x=x, y=y)
         
     def assignwidth(self, event):
         self.pwidth.set(self.wcontrol.get())
+      
+
+    def assigncolor(self, event):
+        r = '%02x' %self.red.get()
+        g = '%02x' %self.green.get()
+        b = '%02x' %self.blue.get()
+        self.color.set('#%s%s%s'%(r, g, b))
+        self.colorbar.config(fg='#%s%s%s'%(r, g, b))
+
+    def defaultset(self):
+        self.wcontrol.set(self.pwidth.get())
+        buffcol = self.color.get()
+        self.red.set(int(buffcol[1:3], base=16))
+        self.green.set(int(buffcol[3:5], base=16))
+        self.blue.set(int(buffcol[5:], base=16))
 
 class DrawBoard(Canvas):
     def __init__(self, root):
@@ -51,7 +88,7 @@ class DrawBoard(Canvas):
         self.y = 0
         self.dialogbox = Label(root)
         self.color = StringVar()
-        self.color.set('white')
+        self.color.set('#ffffff')
         self.pwidth = IntVar()
         self.pwidth.set('50')
         
