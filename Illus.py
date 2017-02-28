f88217d1be4fe19a9e82b36d8516ae6e26bea077
Shubham Mishra <donofsuri@gmail.com>
2017-02-28 12:54:55 +0530

    Board color can be changed now

--- Illus.py
+++ Illus.py
@@ -17,22 +17,16 @@
 from Tkinter import *
 
 class RtDialog(Label):
-    def __init__(self, root, color, pwidth, x, y):
+    def __init__(self, root, color, pwidth, x, y, boardcol):
         Label.__init__(self, root, height=40, width=40)
-        #self.colors = ('White', 'Black', 'Blue', 'Beige',\
-        #               'Cyan', 'Magenta', 'Green', 'Red', 'Orange', 'Brown',\
-        #               'Yellow')
-        #self.menu = apply(OptionMenu, (self, color) + self.colors)
-        #self.menu.pack()
-        self.title1 = Label(self, text='Color\n__________').pack()
-        self.title2 = Label(self, text='Red').pack()
-        self.red = Scale(self, from_=0, to=255, orient=HORIZONTAL)
+ 
+        self.red = Scale(self, from_=0, to=255, orient=HORIZONTAL,label='Red')
         self.red.pack() 
-        self.title3 = Label(self, text='Green').pack()
-        self.green =  Scale(self, from_=0, to=255, orient=HORIZONTAL)
+      
+        self.green =  Scale(self, from_=0, to=255, orient=HORIZONTAL, label='Green')
         self.green.pack()
-        self.title4 = Label(self, text='Blue').pack()
-        self.blue =  Scale(self, from_=0, to=255, orient=HORIZONTAL)
+       
+        self.blue =  Scale(self, from_=0, to=255, orient=HORIZONTAL, label='Blue')
         self.blue.pack()
         self.red.bind('<Button-1>', self.assigncolor)
         self.red.bind('<B1-Motion>', self.assigncolor)
@@ -44,14 +38,14 @@ class RtDialog(Label):
         self.color = color 
         self.pwidth = pwidth
         
-        self.title5 = Label(self, text='Width\n__________').pack()
-        self.wcontrol = Scale(self, from_=1, to=100, orient=HORIZONTAL)
+       
+        self.wcontrol = Scale(self, from_=1, to=100, orient=HORIZONTAL, label='Width')
         self.wcontrol.bind('<Button-1>', self.assignwidth)
         self.wcontrol.bind('<B1-Motion>', self.assignwidth)
         self.wcontrol.pack()
 
         self.defaultset()
-        self.colorbar = Label(self, text='Illus', width=10, background='Black', fg=self.color.get())
+        self.colorbar = Label(self, text='Illus', width=10, background=boardcol, fg=self.color.get())
         self.colorbar.pack()
         
         self.place(x=x, y=y)
@@ -77,12 +71,13 @@ class RtDialog(Label):
 class DrawBoard(Canvas):
     def __init__(self, root):
         self.root = root
-        Canvas.__init__(self, root, background="black")
+        Canvas.__init__(self, root, background="#000000")
         self.configure(height=768, width=1024)
         self.bind("<Button-1>", self.coordreset)
         self.bind("<B1-Motion>", self.draw)
         self.bind("<B2-Motion>", self.erase)
         self.bind("<Button-3>", self.showDialog)
+        
         self.config(scrollregion=self.bbox(ALL))
         self.x = 0
         self.y = 0
@@ -113,20 +108,27 @@ class DrawBoard(Canvas):
                   
     def showDialog(self, event):
         self.dialogdeath()
-        self.dialogbox = RtDialog(self.root, self.color, self.pwidth, event.x, event.y)
+        self.dialogbox = RtDialog(self.root, self.color, self.pwidth, event.x, event.y, self.cget('background'))
+        
     def dialogdeath(self):
         self.dialogbox.destroy()
 
     def coordreset(self, event):
-	    self.x, self.y = self.canvasx(event.x), self.canvasy(event.y)
+        self.x, self.y = self.canvasx(event.x), self.canvasy(event.y)
+
+    def changeBoardColor(self, event):
+        self.configure(background=self.color.get())
         
 
 if __name__!="__main__":
     def createboard(*args):
-        global Board
-        Board.destroy()
-        Board = DrawBoard(root)
-        
+        try:
+            global Board
+            Board.delete(ALL)
+        except:          
+            Board = DrawBoard(root)
+            return Board
+
     def onmousewheel(event):
         if event.num == 4:
             Board.yview_scroll(-1, "units")
@@ -143,12 +145,13 @@ if __name__!="__main__":
     except:
         icon = PhotoImage(file="Illus/icon.png")
     root.tk.call('wm', 'iconphoto', root._w, icon)
-           
-    Board = Label(text='ILLUS\nby Shubham Mishra')
-    Board.pack()
+       
+
     root.title('ILLUS')
     root.bind('<N>', createboard)
-    createboard()
+   
+    Board = createboard()
+    root.bind('<T>', Board.changeBoardColor)
     Board.bind_all("<Button-4>",onmousewheel)
     Board.bind_all("<Button-5>", onmousewheel)
 
